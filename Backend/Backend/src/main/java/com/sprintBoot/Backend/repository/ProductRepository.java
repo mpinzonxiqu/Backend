0c45f3c7f31ab4bc58290d460d29882028a4661d
Moises Pinzon <moisespinzon97@hotmail.com>
2025-04-09 09:18:53 -0500

    Actualizacion

--- Backend/Backend/src/main/java/com/sprintBoot/Backend/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sprintBoot.Backend.repository;
-
-import com.sprintBoot.Backend.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-    // Métodos personalizados si es necesario
-}
